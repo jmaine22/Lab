f622fd38ca628f97a962d155d1037247005d5dcd
Jmaine22 <jmaine22@gmail.com>
2021-02-11 15:38:39 -0800

    init commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let num = prompt('Guess a number between 1 and 10');
+// alert('You guessed the number' + num);
+
+console.log('You guessed the number ' + num);
+
+// Display result within console window
+console.log('You guessed the number ' + num);
+console.info('You guessed the number ' + num);
+console.warn('You guessed the number ' + num);
+console.error('You guessed the number ' + num);
+
+// Display result within web page
+document.write('You guessed the number ' + num);
